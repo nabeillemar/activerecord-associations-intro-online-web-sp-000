5fb6f96ea3de7d0e6b0c021e0eab29fa865a62cc
Nabeil <nabeillemar@gmail.com>
2021-03-06 13:25:56 -0800

    Done.

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
-class CreateSongs < ActiveRecord::Migration[4.2]
+class CreateSongs < ActiveRecord::Migration[4.2] #take in activeRecord Migration 
+    def change 
+        create_table :songs do |t| #creating a table and iterating each column and naming each one 
+            t.string :name
+            t.integer :artist_id
+            t.integer :genre_id
+        end 
+    end 
 end
